89b56dda929462ed1c4f6271fa837c377317bd0f
madalinama <55551916+madalinama@users.noreply.github.com>
2019-12-20 20:08:27 +0200

    Update main.cpp

--- Tema_8.1/main.cpp
+++ Tema_8.1/main.cpp
@@ -25,10 +25,14 @@ int main()
     Cilindru chilly (2,5);
     cout << "Volumul lui chilly este " <<chilly.Volum()<< endl;
     cout << "Aria lui chilly este " <<chilly.Arie()<<endl;
+
     Cilindru* hipster= new Cilindru[2] {Cilindru (2,5), Cilindru(5,2)};
     cout << "Volumul primului hipster este " <<hipster[0].Volum()<< endl;
     cout << "Aria primului hipster este " <<hipster[0].Arie()<<endl;
+
     cout << "Volumul celui de-al doilea hipster este " <<hipster[1].Volum()<< endl;
     cout << "Aria celui de-al doilea hipster este " <<hipster[1].Arie()<<endl;
+
+    delete hipster;
     return 0;
 }
